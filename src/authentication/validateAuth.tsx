9192558eaf82d02ac3f791b9c5b930ea4aa1463e
sebastian2000pk <juochoas@unal.edu.co>
2024-08-14 17:04:52 -0500

    fix: styles

--- src/authentication/validateAuth.tsx
+++ src/authentication/validateAuth.tsx
@@ -1,5 +1,12 @@
+import type { User } from "./user";
+
 export const getToken = async (): Promise<string | null> => {
   const userString = localStorage.getItem("user");
   const user = userString ? JSON.parse(userString) : null;
   return user?.token;
+};
+
+export const getUser = async (): Promise<User> => {
+  const userString = localStorage.getItem("user");
+  return userString ? JSON.parse(userString) : {};
 };
