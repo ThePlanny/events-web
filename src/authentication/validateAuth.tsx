720717fb7e1b086e905672facba2a35e2b2e1023
sebastian2000pk <juochoas@unal.edu.co>
2024-08-14 16:57:37 -0500

    feat: persist session

--- src/authentication/validateAuth.tsx
+++ src/authentication/validateAuth.tsx
@@ -1,15 +1,5 @@
-import { useEffect } from "react";
-import { useAuth } from "./useAuth";
-
-export const ValidateAuth = () => {
-  const userAuthenticated = useAuth((state) => state.userAuthenticated);
-  console.log("🥑 ~ useEffect ~ userAuthenticated:", userAuthenticated);
-
-  useEffect(() => {
-    if (!userAuthenticated) {
-      window.location.href = "/auth/login";
-    }
-  }, [userAuthenticated]);
-
-  return null; // No necesitas renderizar nada, solo manejar la redirección
+export const getToken = async (): Promise<string | null> => {
+  const userString = localStorage.getItem("user");
+  const user = userString ? JSON.parse(userString) : null;
+  return user?.token;
 };
