361a7777a57d4461a84bc4af8d7c7eae82a01f17
sebastian2000pk <juochoas@unal.edu.co>
2024-08-14 23:23:39 -0500

    feat: service to get all athletes

--- src/services/athlete.ts
+++ src/services/athlete.ts
@@ -31,4 +31,30 @@ export class AthleteService {
       id: data.id,
     };
   }
+
+  async getAllAthletes(): Promise<Athlete[]> {
+    const token = await getToken();
+    const response = await fetch(
+      "https://planny-432016.uc.r.appspot.com/api/athletes",
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+        method: "GET",
+      }
+    );
+
+    if (!response.ok) throw new Error("Error fetching athletes");
+
+    const data = await response.json();
+
+    return data.map((athlete: Athlete) => ({
+      birthDate: athlete.birthDate,
+      email: athlete.email,
+      documentNumber: athlete.documentNumber,
+      documentType: athlete.documentType,
+      fullName: athlete.fullName,
+      id: athlete.id,
+    }));
+  }
 }
