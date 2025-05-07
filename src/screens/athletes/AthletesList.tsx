4365f62c72744bf8f9998299b29fe4e93ab90176
WilliGomez <willgom.95@gmail.com>
2025-05-07 15:45:18 -0500

    link the register

--- src/screens/athletes/AthletesList.tsx
+++ src/screens/athletes/AthletesList.tsx
@@ -38,7 +38,7 @@ export const AthletesList = () => {
   async function handleClick() {
     try {
       const newToken = await athleteService.getlink();
-      setToken(newToken);
+      setToken("http://localhost:4321/createUserWithLink?token="+ newToken);
       console.log("Token recibido:", newToken);
     } catch (error) {
       console.error("Error al obtener el token:", error);
